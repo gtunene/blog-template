b643ecbaae5c77d91cd1d1b02329ceceec676a72
gtunene <gerarditomusic@gmail.com>
2024-12-28 22:15:01 +0100

    social icons

--- src/data/links.ts
+++ src/data/links.ts
@@ -1,17 +1,27 @@
 import TwitterIcon from '@/components/icons/TwitterIcon'
 import GithubIcon from '@/components/icons/GithubIcon'
+import InstagramIcon from '@/components/icons/InstagramIcon'
+import FacebookIcon from '@/components/icons/FacebookIcon'
+import YoutubeIcon from '@/components/icons/YoutubeIcon'
+import TiktokIcon from '@/components/icons/TiktokIcon'
 
 // ADD YOUR SOCIAL NETWORKS HERE
 export const SOCIALNETWORKS = [
 	{
-		name: 'Github',
+		name: 'Instagram',
 		url: 'https://github.com/danielcgilibert/blog-template',
-		icon: GithubIcon
+		icon: InstagramIcon
 	},
 
 	{
-		name: 'Twitter',
+		name: 'Tiktok',
 		url: 'https://github.com/danielcgilibert/blog-template',
-		icon: TwitterIcon
+		icon: TiktokIcon
+	},
+
+	{
+		name: 'Youtube',
+		url: 'https://github.com/',
+		icon: YoutubeIcon
 	}
 ] as const
